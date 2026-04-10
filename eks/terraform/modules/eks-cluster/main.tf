d334768e49fe8c5d05f43eca2b1f3e0a141254a7
Bingjiao Yu <yubingjiaocn@gmail.com>
2026-04-10 15:47:06 +0000

    chore: upgrade terraform-aws-modules/eks to ~> 21.0

--- eks/terraform/modules/eks-cluster/main.tf
+++ eks/terraform/modules/eks-cluster/main.tf
@@ -9,7 +9,7 @@ data "aws_iam_session_context" "current" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.24"
+  version = "~> 21.0"
 
   cluster_name    = var.name
   cluster_version = var.cluster_version
